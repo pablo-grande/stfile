62de854e0d7c9366fd6869e0fc276de07ec894a0
Pablo <pablo.riutort@gmail.com>
2018-05-21 22:42:16 +0200

    Add test suite for _configuration_ feature

---
+++ stfile/tests/test_config.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+import stfile
+import stfile.helpers as helpers
+
+
+class TestConfig(TestCase):
+    def test_set_up(self):
+        config, _ = helpers.set_up()
+        self.assertIsInstance(config, dict)
+
+        self.assertIsNotNone(config.get('graph_file'))
+        self.assertIsNotNone(config.get('base_ontology'))
+        self.assertIsNotNone(config.get('namespaces'))
