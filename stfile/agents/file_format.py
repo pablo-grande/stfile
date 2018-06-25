abba919da968d607f9b285f832c1e23cce8e88ca
Pablo <pablo.riutort@gmail.com>
2018-06-25 10:16:47 +0200

    Better exception handling

--- stfile/agents/file_format.py
+++ stfile/agents/file_format.py
@@ -3,16 +3,12 @@ import fleep
 
 def action(graph, namespaces, node_path):
     for node, path in node_path.items():
-        print("tagging ", node, ": ", path)
         try:
             with open(path, 'rb') as f:
                 info = fleep.get(f.read(128))
             type, extension = info.type[0], info.extension[0]
-            print("Extension was found ", extension)
-            print("Type was ",type)
-            print("Setting ",node, " with ", namespaces['']+'fileFormat', namespaces['']+extension.upper())
-            graph.set((node, namespaces['a'], namespaces['nfo']+type.title()))
+            graph.add((node, namespaces['a'], namespaces['nfo']+type.title()))
             graph.set((node, namespaces['']+'fileFormat', namespaces['']+extension.upper()))
-        except e:
-            print(e)
+        except IndexError as e:
+            print("ERROR: Agent file_format could not guess type or extension for", path)
             continue
